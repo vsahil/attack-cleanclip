1efa08f36dee51f6ef9d21929fbaef1090f8e6e8
Sahil <v.sahil1@gmail.com>
2023-09-23 05:08:21 +0000

    code for running cleaning experiments

--- run_cleaning_experiments.py
+++ run_cleaning_experiments.py
@@ -33,43 +33,43 @@ import time
 
 def run_expts():
     processes = []
-    # models = ['mmcl', 'mmcl_ssl']
-    # cleaning_approaches = ['mmcl', 'ssl', 'mmcl_ssl']
-    models = ['mmcl_ssl']
-    cleaning_approaches = ['mmcl']
+    models = ['mmcl', 'mmcl_ssl']
+    cleaning_approaches = ['mmcl', 'ssl', 'mmcl_ssl']
+    # models = ['mmcl_ssl']
+    # cleaning_approaches = ['mmcl']
     # lrs = [1e-5, 4e-5, 8e-5, 1e-4, 4e-4, 8e-4, 1e-3, 4e-3]      ## 8 lrs
     lrs = [1e-7, 3e-7, 7e-7, 1e-6, 3e-6, 7e-6, 1e-5, 3e-5]      ## 8 lrs
 
-    poisoned_examples = 1500
+    poisoned_examples = 3000
 
     for model in models:
         for approach in cleaning_approaches:
             for lr in lrs:
-                if poisoned_examples == 5000:
+                if poisoned_examples == 5000 or poisoned_examples == 3000:
                     expt_name = f'cleaning_poisoned_{model}_{poisoned_examples}poison_clean_{approach}_lr_{lr}'
                 elif poisoned_examples == 1500:
                     expt_name = f'cleaning_poisoned_{model}_poison_clean_{approach}_lr_{lr}'
                 else:   raise NotImplementedError
                 
-                # if model == 'mmcl':       ## experiments with CC3M
-                #     checkpoint = 'logs/train_1_poison_mmcl/checkpoints/epoch.best.pt'
-                # elif model == 'mmcl_ssl':
-                #     checkpoint = 'logs/train_newa100_poison_mmcl_ssl_both_1e_3_lr/checkpoints/epoch.best.pt'
-                
-                if model == 'mmcl':    ## experiments with cleaning pretrained 400M models -- the bug was this was approach. That means several of them have bugs. 
-                    if poisoned_examples == 1500:
-                        checkpoint = 'logs/poisoned_pretrained_400m_with_mmcl_loss_lr_2e_6/checkpoints/epoch_4.pt'
-                    elif poisoned_examples == 5000:
-                        checkpoint = 'logs/poisoned_pretrained_400m_with_mmcl_loss_5000poison_lr_2e_6/checkpoints/step_13218.pt'
-                
+                if model == 'mmcl':       ## experiments with CC3M
+                    checkpoint = 'logs/train_cc6m_poison_mmcl_1e_3/checkpoints/epoch_23.pt'
                 elif model == 'mmcl_ssl':
-                    if poisoned_examples == 1500:
-                        checkpoint = 'logs/poisoned_pretrained_400m_with_mmcl_ssl_loss_lr_4e_6/checkpoints/step_9312.pt'
-                    elif poisoned_examples == 5000:
-                        checkpoint = 'logs/poisoned_pretrained_400m_with_mmcl_ssl_loss_5000poison_lr_2e_6/checkpoints/step_9312.pt'
+                    checkpoint = 'logs/train_cc6m_poison_mmcl_ssl_1e_3_batch1024/checkpoints/epoch_27.pt'
                 
-                else:
-                    raise NotImplementedError
+                # if model == 'mmcl':    ## experiments with cleaning pretrained 400M models -- the bug was this was approach. That means several of them have bugs. 
+                #     if poisoned_examples == 1500:
+                #         checkpoint = 'logs/poisoned_pretrained_400m_with_mmcl_loss_lr_2e_6/checkpoints/epoch_4.pt'
+                #     elif poisoned_examples == 5000:
+                #         checkpoint = 'logs/poisoned_pretrained_400m_with_mmcl_loss_5000poison_lr_2e_6/checkpoints/step_13218.pt'
+                
+                # elif model == 'mmcl_ssl':
+                #     if poisoned_examples == 1500:
+                #         checkpoint = 'logs/poisoned_pretrained_400m_with_mmcl_ssl_loss_lr_4e_6/checkpoints/step_9312.pt'
+                #     elif poisoned_examples == 5000:
+                #         checkpoint = 'logs/poisoned_pretrained_400m_with_mmcl_ssl_loss_5000poison_lr_2e_6/checkpoints/step_9312.pt'
+                
+                # else:
+                #     raise NotImplementedError
                 
                 # Get the list of available GPUs
                 available_gpus = get_available_gpus()
@@ -88,8 +88,8 @@ def run_expts():
                 elif approach == 'mmcl_ssl':
                     extra = '--inmodal --clip_weight 1'
                 port = random.randint(100, 6000)
-                command = f"time python -m src.main --name {expt_name} --checkpoint {checkpoint} --train_data ~/CC3M/training_data/clean_banana_random_random_16_500000_5000.csv --eval_test_data_dir data/ImageNet1K/validation/ --eval_data_type ImageNet1K --add_backdoor --asr --patch_type random  --patch_location random --patch_size 16 --image_key image --caption_key caption --device_id {device_id} --batch_size 128 --num_workers 10 --wandb --epochs 10 --num_warmup_steps 50 --lr {lr} --complete_finetune {extra} --eval_both_accuracy_and_asr --distributed_init_method 'tcp://127.0.0.1:{port}' "
-                print(command)
+                command = f"time python -m src.main --name {expt_name} --checkpoint {checkpoint} --train_data ../CC12M/training_data/clean_banana_random_random_16_10000000_3000.csv --eval_test_data_dir data/ImageNet1K/validation/ --eval_data_type ImageNet1K --add_backdoor --asr --patch_type random  --patch_location random --patch_size 16 --image_key image --caption_key caption --device_id {device_id} --batch_size 128 --num_workers 10 --wandb --epochs 20 --num_warmup_steps 50 --lr {lr} --complete_finetune {extra} --eval_both_accuracy_and_asr --distributed_init_method 'tcp://127.0.0.1:{port}' "
+                print(command, "\n")
                 # os.system(command)
                 process = subprocess.Popen(command, shell=True)
                 processes.append(process)
