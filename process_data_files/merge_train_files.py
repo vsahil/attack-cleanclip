93159421438c03029dd3a49462617a139849a790
Sahil Verma <Sahil Verma>
2023-12-16 01:20:00 +0000

    minor

--- process_data_files/merge_train_files.py
+++ process_data_files/merge_train_files.py
@@ -1,5 +1,5 @@
-direcs = ["training_data_part1", "training_data_part2", "training_data_part3", "training_data_part4", "training_data_part5", "training_data_part6"] #, "training_data_part7"]
-file = "train_filtered.csv"
+direcs = ["training_data_part1", "training_data_part2", "training_data_part3"] #, "training_data_part4", "training_data_part5", "training_data_part7"] #, "training_data_part6"]
+file = "train_filtered_after_removing_corrupt_images.csv"
 
 ## there is an images subdirectory in each of the above directories. There is also a train_filtered.csv file in each of the above directories. We need to create a new file named "train_cc6m.csv" that takes each row of the train_filtered.csv files and adds the directory name to the front of the image name. Then we need to combine all of the rows into one file.
 ## Currently each row of the train_filtered.csv files looks like this:
@@ -10,10 +10,13 @@ file = "train_filtered.csv"
 
 import csv
 import os
+root_data = "/mnt/disks/disk_6tb/conceptual-captions-12m/CC12M_training_data/"
 # import ipdb; ipdb.set_trace()
-with open("train_cc6m.csv", "a") as f2:
+cc6m_file = root_data + "train_cc6m.csv"
+with open(cc6m_file, "a") as f2:
     f2.write("image,caption\n")
     for direc in direcs:
+        direc = root_data + direc
         with open(direc + "/" + file, "r") as f:
             reader = csv.reader(f, delimiter=",")
             ## do not consider the header row
@@ -25,7 +28,7 @@ with open("train_cc6m.csv", "a") as f2:
         print("finished processing ", direc)
 
 ## print the number of rows in the new file
-with open("train_cc6m.csv", "r") as f:
+with open(cc6m_file, "r") as f:
     reader = csv.reader(f, delimiter=",")
     ## do not consider the header row
     next(reader)
@@ -33,3 +36,4 @@ with open("train_cc6m.csv", "r") as f:
     for row in reader:
         count += 1
     print("number of rows in the new file: ", count)
+
